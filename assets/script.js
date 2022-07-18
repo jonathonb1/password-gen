fbf4f019225cd6e06afe3fb41ac1c2b9fec88772
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-07-18 00:49:33 -0700

    Added character arrays and first pw prompt

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,40 @@
+
+// ARRAY OF NUMERIC CHARACTERS
+var numericCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+// ARRAY OF SPECIAL CHARACTERS
+var specialCharacters = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '-', '_', '=', '+', 
+'|', '\\', '/', '?', '.', '>', '<', ','
+];
+
+// ARRAY OF LOWER CASE CHARACTERS
+var lowerCasedCharacters = ['a','b','c','d','e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 
+'s', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+// ARRAY OF UPPER CASE LETTERS
+var uppperCasedCharacters = [  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T',
+'U','V','W','X','Y','Z'
+];
+// -------------------------------------
+
+// PROMPT USER FOR PASSWORD OPTIONS
+function getPasswordOptions() {
+  // VARIABLE TO STORE LENGTH OF PASSWORD
+  var length = parseInt(
+    prompt('How many characters would you like your password to have?')
+  );
+
+// CONDITIONAL STATEMENT CHEKCING IF PW LENGTH IS A NUMBER.  PROMPT WILL END OF FALSE
+if (isNaN(length)===true) {
+  alert('Password length must be numeric');
+  return;
+}
+};
+
+console.log(getPasswordOptions)
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
