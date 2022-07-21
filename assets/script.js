1adabbec52f295e095d492b9bdb18fc837e1b3d5
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-07-21 13:42:02 -0700

    generator objects

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,39 @@
 // Assignment code here
 
+// DOM elements
+
+var randomFunc = {
+  length: length,
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+
+// Generator functions - https://www.net-comber.com/charset.html (codes for character sets)
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol(){
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+var 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +45,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+
+}
